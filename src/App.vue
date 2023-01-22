bad9c69dc543709577c8bbb771b571d5fb033a30
liying <v_liying38@baidu.com>
2023-01-22 16:13:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+const msg = ref('vite + vue')
+
+function updateMsg (val){
+  console.log(val)
+  msg.value = val
+}
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld :msg="msg"  @updateMsg = "updateMsg"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+const emit = defineEmits('updateMsg')
+
+const count = ref(0)
+const inputValue = ref('')
+
+
+
+onMounted(()=>{
+  emit('updateMsg', '123')
+})
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <input v-model="inputValue"/>
+  <button @click="$emit('updateMsg', inputValue)">修改msg</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
